fefb25e612835dc4a0dc37312e7d8fc2f9ebaa6b
Marcus Brohede (his) <a97marbr@users.noreply.github.com>
2019-01-22 12:02:01 +0100

    Listview

--- app/src/main/java/com/example/brom/activitiesapp/MainActivity.java
+++ app/src/main/java/com/example/brom/activitiesapp/MainActivity.java
@@ -2,17 +2,37 @@ package com.example.brom.activitiesapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     private String[] mountainNames = {"Matterhorn","Mont Blanc","Denali"};
     private String[] mountainLocations = {"Alps","Alps","Alaska"};
     private int[] mountainHeights ={4478,4808,6190};
-    // Create ArrayLists from the raw data above and use these lists when populating your ListView.
+    Mountain m = new Mountain("Matterhorn", "Alps", 4478);
+    Mountain m2 = new Mountain("Mont Blanc", "Alps", 4808);
+    Mountain m3 = new Mountain("Denali", "Alaska", 6190);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayList<String> berg = new ArrayList<String>(Arrays.asList(mountainNames));
+
+        ArrayList<Mountain> berg2=new ArrayList<>();
+
+
+
+            berg2.add(m);
+            berg2.add(m2);
+            berg2.add(m3);
+
+            ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, R.layout.list_item_textview, R.id.fridasItem, berg);
+            ListView lista= findViewById(R.id.fridaslist);
+            lista.setAdapter(adapter1);
 
         // 1. Create a ListView as in previous assignment
         // 2. Create a new activity named "MountainDetailsActivity
